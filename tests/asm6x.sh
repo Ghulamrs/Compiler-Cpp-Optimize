Ghulamrs/Compiler-Cppi

The tms6747 target reaches an object on any host through asm6x, and a .out through lnk6x
-arch tms6747 -c hands what the compiler wrote to asm6x, the project's
C6000 assembler (CXX1_AS, else the one beside cxx1i, else on PATH), and
writes a .obj; without -c the objects go to TI's lnk6x - CXX1_TI names
CCS's C6000 compiler directory, CXX1_TILIB one holding rts6740_elf_eh.lib,
CXX1_LD the linker - with the linker command file RIDE and VM6747 link
with, into a .out. tests/asm6x.sh holds every case's driver-made object
to asm6x's own (309/309 on the Mac and Linux); tools/windows/ti-link.cmd
links every case on the box (308/308), and TriLab's three-file C++ lab
links the same way; verify-three runs both.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

tests/asm6x.sh
@@ -1,0 +1,57 @@
+#!/bin/sh
+# The tms6747 target through asm6x, the project's own C6000 assembler: every
+# case tests/tms6747.sh would compile is taken to a TI object by the driver
+# itself (`cxx1i -arch tms6747 -c`, which runs asm6x on what it wrote), and
+# that object must be the one asm6x writes for the same assembly by hand -
+# every table of it, the file symbol aside, which names the temporary. What
+# this checks is the driver's path to the assembler, on any host; that asm6x's
+# objects are TI's is ASM6x's own suite, and that TI links them is
+# tools/windows/ti-link.cmd on the box, where lnk6x is.
+#
+#   ASM6X=<asm6x> sh tests/asm6x.sh        the assembler: $ASM6X, else ASM6x's
+#                                          build beside this tree, else one on PATH
+set -u
+cd "$(dirname "$0")/.."
+CXX1="${CXX1:-./cxx1i.exe}"
+ASM6X="${ASM6X:-}"
+if [ -z "$ASM6X" ]; then
+    for c in ../../ASM6x/build/asm6x.exe "$HOME/asm6x-build/asm6x.exe" "$(command -v asm6x 2>/dev/null)"; do
+        [ -n "$c" ] && [ -x "$c" ] && { ASM6X=$c; break; }
+    done
+fi
+[ -n "$ASM6X" ] || { echo "asm6x.sh: no asm6x - name it with ASM6X=<path>"; exit 2; }
+C6XDIFF="${C6XDIFF:-../../ASM6x/tests/c6xdiff.py}"
+[ -f "$C6XDIFF" ] || C6XDIFF="$HOME/asm6x-build/tests/c6xdiff.py"
+[ -f "$C6XDIFF" ] || { echo "asm6x.sh: no c6xdiff.py - name it with C6XDIFF=<path>"; exit 2; }
+OUT=tests/out-asm6x
+rm -rf "$OUT"; mkdir -p "$OUT"
+
+pass=0; fail=0; skip=0
+for src in tests/cases/*.cpp; do
+    base=$(basename "$src" .cpp)
+    case "$base" in *" "*) continue;; esac
+    [ -f "tests/cases/$base.error" ] && continue
+    if [ -f "tests/cases/$base.notarget" ] && grep -q "^tms6747[[:space:]]" "tests/cases/$base.notarget"; then
+        skip=$((skip + 1)); continue
+    fi
+    if ! ( ulimit -t 20; CXX1_AS="$ASM6X" "$CXX1" -arch tms6747 -nologo -c "$src" -o "$OUT/$base.obj" < /dev/null ) 2>"$OUT/$base.err"; then
+        echo "FAIL $base: the driver did not make an object"
+        sed 's/^/      /' "$OUT/$base.err" | head -3
+        fail=$((fail + 1)); continue
+    fi
+    "$CXX1" -arch tms6747 -nologo -S "$src" -o "$OUT/$base.s" < /dev/null 2>/dev/null
+    if ! "$ASM6X" "$OUT/$base.s" -o "$OUT/$base.hand.obj" > "$OUT/$base.hand.err" 2>&1; then
+        echo "FAIL $base: asm6x refused the assembly by hand"
+        sed 's/^/      /' "$OUT/$base.hand.err" | head -3
+        fail=$((fail + 1)); continue
+    fi
+    if python3 "$C6XDIFF" "$OUT/$base.obj" "$OUT/$base.hand.obj" > "$OUT/$base.diff"; then
+        pass=$((pass + 1))
+    else
+        echo "FAIL $base: the driver's object is not asm6x's"
+        sed 's/^/      /' "$OUT/$base.diff" | head -6
+        fail=$((fail + 1))
+    fi
+done
+echo "asm6x.sh: $pass objects through the driver as by hand, $fail failed, $skip not for this target"
+[ "$fail" -eq 0 ]

tools/windows/ti-link.cmd
@@ -1,0 +1,43 @@
+@echo off
+rem  The tms6747 target taken all the way to a TI program by the driver: every
+rem  case run-cases.cmd compiles is compiled with `cxx1 -arch tms6747`, which
+rem  assembles what it wrote with asm6x and links the object with TI's lnk6x
+rem  against rts6740_elf_eh.lib into <case>.out. The linker is the judge: it
+rem  takes every object asm6x wrote, or names the one it does not.
+rem    ti-link.cmd <tree root>
+rem  asm6x.exe is ASM6x's own cl build (tests/windows.sh leaves it at
+rem  C:\asm6x-tests\build) or the one in RIDE's bin; the TI tools are CCS 7.4's.
+setlocal enabledelayedexpansion
+if "%~1"=="" (echo ti-link.cmd: needs the tree root & exit /b 2)
+set ROOT=%~1
+set CXX1_TI=C:\ti\ccsv7\tools\compiler\ti-cgt-c6000_8.2.2
+set CXX1_TILIB=C:\Users\GRA\Documents\VM6747\tilib
+set CXX1_AS=C:\asm6x-tests\build\asm6x.exe
+if not exist %CXX1_AS% set CXX1_AS=C:\Users\GRA\source\RStudio\bin\asm6x.exe
+if not exist %CXX1_AS% (echo ti-link.cmd: no asm6x.exe & exit /b 1)
+if not exist %CXX1_TI%\bin\lnk6x.exe (echo ti-link.cmd: no lnk6x under %CXX1_TI% & exit /b 1)
+if not exist %CXX1_TILIB%\rts6740_elf_eh.lib (echo ti-link.cmd: no rts6740_elf_eh.lib - see Emulator/tests/ti.sh & exit /b 1)
+if not exist %ROOT%\cxx1-msvc.exe (echo ti-link.cmd: no cxx1-msvc.exe - run-cases.cmd builds it & exit /b 1)
+if not exist %ROOT%\winout\ti mkdir %ROOT%\winout\ti
+del /q %ROOT%\winout\ti\* 2>nul
+set linked=0
+set failed=0
+set skipped=0
+for %%f in (%ROOT%\tests\cases\*.expected) do (
+    set NAME=%%~nf
+    set SKIP=
+    if exist %ROOT%\tests\cases\!NAME!.notarget (
+        findstr /C:"tms6747" %ROOT%\tests\cases\!NAME!.notarget >nul 2>&1 && set SKIP=1
+    )
+    if defined SKIP (
+        set /a skipped+=1
+    ) else (
+        %ROOT%\cxx1-msvc.exe -arch tms6747 -nologo %ROOT%\tests\cases\!NAME!.cpp -o %ROOT%\winout\ti\!NAME!.out > %ROOT%\winout\ti\!NAME!.log 2>&1
+        if errorlevel 1 (set /a failed+=1 & echo TI-FAILED !NAME! & type %ROOT%\winout\ti\!NAME!.log | findstr /v "^$" | more +0) else (
+            if exist %ROOT%\winout\ti\!NAME!.out (set /a linked+=1) else (set /a failed+=1 & echo TI-NO-OUT !NAME!)
+        )
+    )
+)
+echo ti-link.cmd: %linked% programs linked by lnk6x, %failed% failed, %skipped% not for this target
+if not %failed%==0 exit /b 1
+endlocal

Ghulamrs/Compiler-Cpp-Optimize

(cherry picked from commit 2489b9e3eebcab886654db0a65257f6335695363)

tests/asm6x.sh
@@ -1,0 +1,57 @@
+#!/bin/sh
+# The tms6747 target through asm6x, the project's own C6000 assembler: every
+# case tests/tms6747.sh would compile is taken to a TI object by the driver
+# itself (`cpp11 -arch tms6747 -c`, which runs asm6x on what it wrote), and
+# that object must be the one asm6x writes for the same assembly by hand -
+# every table of it, the file symbol aside, which names the temporary. What
+# this checks is the driver's path to the assembler, on any host; that asm6x's
+# objects are TI's is ASM6x's own suite, and that TI links them is
+# tools/windows/ti-link.cmd on the box, where lnk6x is.
+#
+#   ASM6X=<asm6x> sh tests/asm6x.sh        the assembler: $ASM6X, else ASM6x's
+#                                          build beside this tree, else one on PATH
+set -u
+cd "$(dirname "$0")/.."
+CXX1="${CXX1:-./cpp11.exe}"
+ASM6X="${ASM6X:-}"
+if [ -z "$ASM6X" ]; then
+    for c in ../../ASM6x/build/asm6x.exe "$HOME/asm6x-build/asm6x.exe" "$(command -v asm6x 2>/dev/null)"; do
+        [ -n "$c" ] && [ -x "$c" ] && { ASM6X=$c; break; }
+    done
+fi
+[ -n "$ASM6X" ] || { echo "asm6x.sh: no asm6x - name it with ASM6X=<path>"; exit 2; }
+C6XDIFF="${C6XDIFF:-../../ASM6x/tests/c6xdiff.py}"
+[ -f "$C6XDIFF" ] || C6XDIFF="$HOME/asm6x-build/tests/c6xdiff.py"
+[ -f "$C6XDIFF" ] || { echo "asm6x.sh: no c6xdiff.py - name it with C6XDIFF=<path>"; exit 2; }
+OUT=tests/out-asm6x
+rm -rf "$OUT"; mkdir -p "$OUT"
+
+pass=0; fail=0; skip=0
+for src in tests/cases/*.cpp; do
+    base=$(basename "$src" .cpp)
+    case "$base" in *" "*) continue;; esac
+    [ -f "tests/cases/$base.error" ] && continue
+    if [ -f "tests/cases/$base.notarget" ] && grep -q "^tms6747[[:space:]]" "tests/cases/$base.notarget"; then
+        skip=$((skip + 1)); continue
+    fi
+    if ! ( ulimit -t 20; CPP11_AS="$ASM6X" "$CXX1" -arch tms6747 -nologo -c "$src" -o "$OUT/$base.obj" < /dev/null ) 2>"$OUT/$base.err"; then
+        echo "FAIL $base: the driver did not make an object"
+        sed 's/^/      /' "$OUT/$base.err" | head -3
+        fail=$((fail + 1)); continue
+    fi
+    "$CXX1" -arch tms6747 -nologo -S "$src" -o "$OUT/$base.s" < /dev/null 2>/dev/null
+    if ! "$ASM6X" "$OUT/$base.s" -o "$OUT/$base.hand.obj" > "$OUT/$base.hand.err" 2>&1; then
+        echo "FAIL $base: asm6x refused the assembly by hand"
+        sed 's/^/      /' "$OUT/$base.hand.err" | head -3
+        fail=$((fail + 1)); continue
+    fi
+    if python3 "$C6XDIFF" "$OUT/$base.obj" "$OUT/$base.hand.obj" > "$OUT/$base.diff"; then
+        pass=$((pass + 1))
+    else
+        echo "FAIL $base: the driver's object is not asm6x's"
+        sed 's/^/      /' "$OUT/$base.diff" | head -6
+        fail=$((fail + 1))
+    fi
+done
+echo "asm6x.sh: $pass objects through the driver as by hand, $fail failed, $skip not for this target"
+[ "$fail" -eq 0 ]

tools/windows/ti-link.cmd
@@ -1,0 +1,43 @@
+@echo off
+rem  The tms6747 target taken all the way to a TI program by the driver: every
+rem  case run-cases.cmd compiles is compiled with `cxx1 -arch tms6747`, which
+rem  assembles what it wrote with asm6x and links the object with TI's lnk6x
+rem  against rts6740_elf_eh.lib into <case>.out. The linker is the judge: it
+rem  takes every object asm6x wrote, or names the one it does not.
+rem    ti-link.cmd <tree root>
+rem  asm6x.exe is ASM6x's own cl build (tests/windows.sh leaves it at
+rem  C:\asm6x-tests\build) or the one in RIDE's bin; the TI tools are CCS 7.4's.
+setlocal enabledelayedexpansion
+if "%~1"=="" (echo ti-link.cmd: needs the tree root & exit /b 2)
+set ROOT=%~1
+set CPP11_TI=C:\ti\ccsv7\tools\compiler\ti-cgt-c6000_8.2.2
+set CPP11_TILIB=C:\Users\GRA\Documents\VM6747\tilib
+set CPP11_AS=C:\asm6x-tests\build\asm6x.exe
+if not exist %CPP11_AS% set CPP11_AS=C:\Users\GRA\source\RStudio\bin\asm6x.exe
+if not exist %CPP11_AS% (echo ti-link.cmd: no asm6x.exe & exit /b 1)
+if not exist %CPP11_TI%\bin\lnk6x.exe (echo ti-link.cmd: no lnk6x under %CPP11_TI% & exit /b 1)
+if not exist %CPP11_TILIB%\rts6740_elf_eh.lib (echo ti-link.cmd: no rts6740_elf_eh.lib - see Emulator/tests/ti.sh & exit /b 1)
+if not exist %ROOT%\cxx1-msvc.exe (echo ti-link.cmd: no cxx1-msvc.exe - run-cases.cmd builds it & exit /b 1)
+if not exist %ROOT%\winout\ti mkdir %ROOT%\winout\ti
+del /q %ROOT%\winout\ti\* 2>nul
+set linked=0
+set failed=0
+set skipped=0
+for %%f in (%ROOT%\tests\cases\*.expected) do (
+    set NAME=%%~nf
+    set SKIP=
+    if exist %ROOT%\tests\cases\!NAME!.notarget (
+        findstr /C:"tms6747" %ROOT%\tests\cases\!NAME!.notarget >nul 2>&1 && set SKIP=1
+    )
+    if defined SKIP (
+        set /a skipped+=1
+    ) else (
+        %ROOT%\cxx1-msvc.exe -arch tms6747 -nologo %ROOT%\tests\cases\!NAME!.cpp -o %ROOT%\winout\ti\!NAME!.out > %ROOT%\winout\ti\!NAME!.log 2>&1
+        if errorlevel 1 (set /a failed+=1 & echo TI-FAILED !NAME! & type %ROOT%\winout\ti\!NAME!.log | findstr /v "^$" | more +0) else (
+            if exist %ROOT%\winout\ti\!NAME!.out (set /a linked+=1) else (set /a failed+=1 & echo TI-NO-OUT !NAME!)
+        )
+    )
+)
+echo ti-link.cmd: %linked% programs linked by lnk6x, %failed% failed, %skipped% not for this target
+if not %failed%==0 exit /b 1
+endlocal